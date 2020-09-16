972d4af1282b1ad5810b80fe9cfb358b57b24a21
Maximus Shymko <maximus.shymko@polymtl.ca>
2020-09-16 15:01:52 -0400

    Correction

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def get_nb_char(string: str, char: str) -> int:
 
 
 def get_nb_words(sentence: str) -> int:
-    return string.count(' ') + 1
+    return sentence.count(' ') + 1
 
 
 def main() -> None:
